d2cee86d11e208c887170d2a89d7f5344561cc09
Deep Shah <66032186+deepsh1210@users.noreply.github.com>
2021-09-16 16:47:09 +0530

    Update home.php

--- home.php
+++ home.php
@@ -15,6 +15,7 @@
 	<title>College Search: Find Best colleges in India</title>
 	<meta name="description" content="A one stop destination to get all information about top colleges and universities in India">
 	<meta name="keywords" content="College Search, Explore Top universities, Admissions, Find Your College">
+	<meta name="robots" content="index, follow">
 	<style>
 	
 	.pagi{
